51b0efa2db14227c7d44168b0b0f31251e7942e3
Becky <yrstudio9316@gmail.com>
2017-01-19 15:50:56 +0800

    Add static todo list

---
+++ app/components/TodoList.jsx
@@ -0,0 +1,27 @@
+var React = require("react");
+var Todo = require("Todo");
+
+var TodoList = React.createClass({
+    render: function () {
+        var {todos} = this.props;
+        var renderTodos = () => {
+            return todos.map((todo) => {
+                // when you iterate over an array and generate multiple instances of component
+                // you have to give them a unique key prop
+                // this key prop is used internally by react to keep track of the individual component
+                // ...todo : spread operator; the same as todo = {todo}
+                //           使用展開運算符(...)在指定props的值上，是另一種更加簡化的語法，
+                //           它是把已有的props物件值展開來指定這個元件中
+                //           pass down every attribute in the todo object
+                //           as props to the react component without explicity defined everything
+                return <Todo key={todo.id} {...todo}/>
+            });
+        };
+
+        return (
+            <div>{renderTodos()}</div>
+        );
+    }
+});
+
+module.exports = TodoList;
