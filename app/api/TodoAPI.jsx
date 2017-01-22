7d86047fddcd36b480986a9aa37dcbba49f2d16f
Becky <yrstudio9316@gmail.com>
2017-01-22 10:40:35 +0800

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -25,5 +25,33 @@ module.exports = {
         // } else {
         //     return [];
         // }
+    },
+    filterTodos: function (todos, showCompleted, searchText) {
+        var filteredTodos = todos;
+
+        // Filter by showCompleted
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });
+
+        // Filter by searchText
+        filteredTodos = filteredTodos.filter((todo) => {
+            var text = todo.text.toLowerCase();
+            // searchText.length === 0 means that there's no input in search field
+            // so we return all text found in todo
+            return searchText.length === 0 || text.indexOf(searchText) > -1;
+        });
+
+        // Sort todos with non-completed first
+        filteredTodos.sort((a, b) => {
+            if (!a.completed && b.completed) {
+                return -1; // a comes before b
+            } else if (a.completed && !b.completed) {
+                return 1; // a comes after b
+            } else {
+                return 0; // no resort (because a = b)
+            }
+        });
+        return filteredTodos;
     }
 };
